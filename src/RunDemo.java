7efb9df2c95088bf3e8c5a7e9f16dc88671b50e1
radeshshiwlochan <radesh0430@gmail.com>
2016-10-15 22:54:22 -0400

    adds a start method

--- src/RunDemo.java
+++ src/RunDemo.java
@@ -1,4 +1,33 @@
 
-public class RunDemo {
+public class RunDemo implements Runnable {
+	public Thread thread;
+	public String nameOfThread;
+	
+	RunDemo(String nameOfThread) {
+		nameOfThread = nameOfThread;
+		System.out.println("In constructor to create " + nameOfThread);
+	}
+	
+	public void run() {
+		System.out.println("In Run Method " + nameOfThread);
+		
+		try {
+			for(int i = 4; i > 0; i--) {
+				System.out.println("This is thread " + nameOfThread + " , " + i);
+				Thread.sleep(3000);
+			}
+		} catch(InterruptedException exception) {
+			System.out.println("This is the thread that was interrupted " + nameOfThread);
+		}
+		System.out.print("Thread exiting : " + nameOfThread);
+	}
+	
+	public void start() {
+		System.out.println("Starting " + nameOfThread);
+		if(thread == null) {
+			thread = new Thread(this, nameOfThread);
+			thread.start();
+		}
+	}
 
 }
